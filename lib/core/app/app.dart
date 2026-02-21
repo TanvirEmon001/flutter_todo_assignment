24742fe0da0f534fd172f4980591b52ed644abff
tanviremon001 <tanvirofficial121@gmail.com>
2026-02-22 00:39:40 +0600

    Initial Flutter project setup

---
+++ lib/core/app/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:todo_management/core/app/app.dart';
+
+void main(){
+  runApp(const TodoApp());
+}
